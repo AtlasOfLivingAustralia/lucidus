6d6cc2b24e97bebcacf5c1266faefc08feb54df1
cjwaite23 <callumwaite2000@gmail.com>
2023-08-15 08:53:40 +0930

    Initialise {renv} and data processing

---
+++ data/data_processing.R
@@ -0,0 +1,12 @@
+##### Description #####
+# The function of this script is to clean the initial dataset of 2001-05 
+# records into a tibble that can be used with shiny to produce the intended
+# visualisations.
+
+##### Libraries #####
+{
+  library(arrow)
+  library(tidyverse)
+}
+
+##### Load Data #####
